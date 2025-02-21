ec725e94d61237228646dd4cd166a09b486e52d7
Akash4056 <asaha8313@gmail.com>
2025-02-21 19:30:08 +0530

    feat: update application title to TaskFlow; rename Navbar button to reflect new branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                             {navItems}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">TaskFlow</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
